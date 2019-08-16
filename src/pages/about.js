973d7a8a383ec1bebfe1fef8714da5f19f063560
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2019-08-16 14:08:01 +1000

    scaffold out gatsby site using cli add about and team pages

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+const About = () => (
+    <Layout>
+        <h1>About Us</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit voluptas nihil officiis. Sunt explicabo porro, deleniti quam harum nisi, perspiciatis temporibus ipsam non optio provident veritatis? Cum alias repellendus dolor?</p>
+    </Layout>
+)
+
+export default About;
---
+++ src/pages/company/team.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '../../components/layout';
+
+const Team = () => (
+    <Layout>
+        <h1>The Team</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit voluptas nihil officiis. Sunt explicabo porro, deleniti quam harum nisi, perspiciatis temporibus ipsam non optio provident veritatis? Cum alias repellendus dolor?</p>
+    </Layout>
+)
+
+export default Team;
