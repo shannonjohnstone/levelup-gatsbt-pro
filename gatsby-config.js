420355e84e1834e43bdd5f40a60381c660374e78
Shannon Johnstone <shannon@shannonjohnstone.com.au>
2019-08-18 14:49:11 +1000

    config update and Arhcive fix gatsby-config incorrect config for filesystem add in arhcive component and logic

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `My First Gatsby Site`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Our most amazing Gatsby generated static site.`,
+    author: `@hekkno`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -11,6 +11,13 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts`,
       },
     },
     `gatsby-transformer-sharp`,
@@ -27,6 +34,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-transformer-remark`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Layout from '../components/layout';
+import SEO from '../components/seo';
 
 const About = () => (
     <Layout>
+        <SEO title="About" description="This is the most amazing About Us page ever!!" />
         <h1>About Us</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit voluptas nihil officiis. Sunt explicabo porro, deleniti quam harum nisi, perspiciatis temporibus ipsam non optio provident veritatis? Cum alias repellendus dolor?</p>
     </Layout>
